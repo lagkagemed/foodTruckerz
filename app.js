0f3a617a309b4f5d8e560080d1b2aa05e90f1828
Kennet Jonas Bonnicksen <kennet_jonas@hotmail.com>
2021-08-25 11:28:39 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+//express setup
+import express from 'express';
+import * as http from 'http';
+import { dirname } from 'path';
+import { Server } from 'socket.io';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+let app = express();
+let serv = http.Server(app);
+
+app.get('/',function(req, res){
+    res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client',express.static(__dirname + '/client'));
+
+serv.listen(process.env.PORT || 2000);
+console.log('Server started.');
